cfeb6f71a272823e52d123f4be9e2852fb93c6eb
Allef Rodrigo Schmidt <allefr@outlook.com>
2016-04-12 02:14:22 -0300

    TP2
    Trabalho de Computação

---
+++ concurso.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct info_Aluno {
+	int dificuldade;
+	int inicio;
+	int fim;
+};
+
+int main() {
+	
+	int no_Participantes, no_Questoes;
+	cout << "Qual o numero de participantes? ";
+	cin >> no_Participantes;
+	cout << "Qual o numero de quetoes? ";
+	cin >> no_Questoes;
+	int *n_Questoes = new int[no_Questoes];
+	info_Aluno * pj = new info_Aluno[no_Participantes];
+
+	char letra = 'A';
+
+	cout << "======================" << endl;
+	for (int i = 0; i < no_Participantes; i++) {
+		cout << "Participante " << i + 1 << endl;
+		cout << "-------------" << endl;
+		for (int x = 0; x < no_Questoes; x++){
+		cout << "Questao " << letra << endl;
+		letra = char(letra + 1);
+		cout << "Dificuldade: ";
+		cin >> pj[i].dificuldade;
+		}
+		letra = 'A';
+		cout << "===============" << endl;
+
+	}
+	cout << "mostrando dificuldades: " << endl;
+		for (int i = 0; i < no_Participantes; i++) {
+			cout << "Participante " << i + 1 << ": " << pj[i].dificuldade << endl;
+	}
+	system("pause");
+	return 0;
+
+}
